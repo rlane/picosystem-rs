66914ba679f7b102d3f114d4ff7b5189f9a25f7c
Rich Lane <lanerl@gmail.com>
2021-10-10 22:06:38 -0700

    display: add a framebuffer

--- src/bin/display.rs
+++ src/bin/display.rs
@@ -19,25 +19,26 @@ pub static BOOT_LOADER: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 fn main() -> ! {
     let mut hw = hardware::Hardware::new();
 
-    info!("Drawing");
-    let circle1 =
-        Circle::new(Point::new(128, 64), 64).into_styled(PrimitiveStyle::with_fill(Rgb565::RED));
-    let circle2 = Circle::new(Point::new(64, 64), 64)
-        .into_styled(PrimitiveStyle::with_stroke(Rgb565::GREEN, 1));
-    hw.display.draw_target().clear(Rgb565::BLACK).unwrap();
-    circle1.draw(hw.display.draw_target()).unwrap();
-    circle2.draw(hw.display.draw_target()).unwrap();
-    hw.display.enable_backlight();
-
     info!("Finished initialization");
     let mut i = 0;
     loop {
-        info!("Info log {}", i);
-        log::error!("Error log");
-        hw.blue_led_pin.set_high().unwrap();
-        hw.delay.delay_ms(500);
-        hw.blue_led_pin.set_low().unwrap();
-        hw.delay.delay_ms(500);
+        hw.display.clear(Rgb565::BLACK).unwrap();
+        let circle1 = Circle::new(Point::new((i + 64) % 240, 64), 64)
+            .into_styled(PrimitiveStyle::with_fill(Rgb565::RED));
+        let circle2 = Circle::new(Point::new(i % 240, 64), 64)
+            .into_styled(PrimitiveStyle::with_stroke(Rgb565::GREEN, 1));
+        circle1.draw(&mut hw.display).unwrap();
+        circle2.draw(&mut hw.display).unwrap();
+        hw.display.flush();
+
+        if i % 2 == 0 {
+            hw.blue_led_pin.set_high().unwrap();
+        } else {
+            hw.blue_led_pin.set_low().unwrap();
+        }
+        if i % 30 == 0 {
+            info!("Frame {}", i);
+        }
         i += 1;
     }
 }
--- src/display.rs
+++ src/display.rs
@@ -1,4 +1,10 @@
+use core::convert::TryInto;
 use display_interface_spi::SPIInterface;
+use embedded_graphics::draw_target::DrawTarget;
+use embedded_graphics::{
+    pixelcolor::{raw::RawU16, Rgb565},
+    prelude::*,
+};
 use embedded_hal::blocking::delay::DelayUs;
 use embedded_hal::digital::v2::OutputPin;
 use embedded_hal::spi::MODE_0;
@@ -12,12 +18,16 @@ use rp2040_hal::gpio::dynpin::DynPin;
 use rp2040_hal::gpio::dynpin::DynPinMode;
 use st7789::ST7789;
 
+const WIDTH: usize = 240;
+const HEIGHT: usize = 240;
+
 pub type RealDisplay =
     ST7789<SPIInterface<Spi<hal::spi::Enabled, pac::SPI0, 8>, DynPin, DynPin>, DynPin>;
 
 pub struct Display {
     st7789: RealDisplay,
     backlight_pin: DynPin,
+    framebuffer: [u16; WIDTH * HEIGHT],
 }
 
 impl Display {
@@ -52,19 +62,52 @@ impl Display {
             &MODE_0,
         );
         let di = SPIInterface::new(spi, lcd_dc_pin, lcd_cs_pin);
-        let mut st7789 = ST7789::new(di, lcd_reset_pin, 240, 240);
+        let mut st7789 = ST7789::new(di, lcd_reset_pin, WIDTH as u16, HEIGHT as u16);
         st7789.init(delay_source).unwrap();
-        Display {
+        st7789.clear(Rgb565::BLACK).unwrap();
+        let mut display = Display {
             st7789,
             backlight_pin,
-        }
+            framebuffer: [0; WIDTH * HEIGHT],
+        };
+        display.enable_backlight();
+        display
     }
 
-    pub fn draw_target(&mut self) -> &mut RealDisplay {
-        &mut self.st7789
+    pub fn flush(&mut self) {
+        self.st7789
+            .set_pixels(0, 0, WIDTH as u16, HEIGHT as u16, self.framebuffer)
+            .unwrap();
     }
 
     pub fn enable_backlight(&mut self) {
         self.backlight_pin.set_high().unwrap();
+    }
+}
+
+impl DrawTarget for Display {
+    type Color = Rgb565;
+    type Error = core::convert::Infallible;
+
+    fn draw_iter<I>(&mut self, pixels: I) -> Result<(), Self::Error>
+    where
+        I: IntoIterator<Item = Pixel<Self::Color>>,
+    {
+        const M: u32 = WIDTH as u32 - 1;
+        for Pixel(coord, color) in pixels.into_iter() {
+            if let Ok((x @ 0..=M, y @ 0..=M)) = coord.try_into() {
+                let index: u32 = x + y * WIDTH as u32;
+                let color = RawU16::from(color).into_inner();
+                self.framebuffer[index as usize] = color;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl OriginDimensions for Display {
+    fn size(&self) -> Size {
+        Size::new(WIDTH as u32, HEIGHT as u32)
     }
 }
