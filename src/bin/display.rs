54c861d57bec2b8359981db5a7ee9319c66b35f3
Rich Lane <lanerl@gmail.com>
2021-10-13 18:32:36 -0700

    display: flush using DMA

--- src/bin/display.rs
+++ src/bin/display.rs
@@ -23,6 +23,18 @@ fn main() -> ! {
     let mut i = 0;
     loop {
         hw.display.clear(Rgb565::BLACK).unwrap();
+        for i in 0..240 {
+            if i % 16 == 0 {
+                let line = Line::new(Point::new(i, 0), Point::new(i, 239))
+                    .into_styled(PrimitiveStyle::with_stroke(Rgb565::GREEN, 1));
+                line.draw(&mut hw.display).unwrap();
+            }
+            if i % 16 == 0 {
+                let line = Line::new(Point::new(0, i), Point::new(239, i))
+                    .into_styled(PrimitiveStyle::with_stroke(Rgb565::BLUE, 1));
+                line.draw(&mut hw.display).unwrap();
+            }
+        }
         let circle1 = Circle::new(Point::new((i + 64) % 240, 64), 64)
             .into_styled(PrimitiveStyle::with_fill(Rgb565::RED));
         let circle2 = Circle::new(Point::new(i % 240, 64), 64)
--- src/display.rs
+++ src/display.rs
@@ -1,5 +1,5 @@
 use core::convert::TryInto;
-use display_interface_spi::SPIInterface;
+use display_interface_spi::SPIInterfaceNoCS;
 use embedded_graphics::draw_target::DrawTarget;
 use embedded_graphics::{
     pixelcolor::{raw::RawU16, Rgb565},
@@ -7,7 +7,7 @@ use embedded_graphics::{
 };
 use embedded_hal::blocking::delay::DelayUs;
 use embedded_hal::digital::v2::OutputPin;
-use embedded_hal::spi::MODE_0;
+use embedded_hal::spi::MODE_3;
 use embedded_time::rate::*;
 use hal::pac;
 use hal::spi::Spi;
@@ -22,7 +22,7 @@ const WIDTH: usize = 240;
 const HEIGHT: usize = 240;
 
 pub type RealDisplay =
-    ST7789<SPIInterface<Spi<hal::spi::Enabled, pac::SPI0, 8>, DynPin, DynPin>, DynPin>;
+    ST7789<SPIInterfaceNoCS<Spi<hal::spi::Enabled, pac::SPI0, 8>, DynPin>, DynPin>;
 
 pub struct Display {
     st7789: RealDisplay,
@@ -47,6 +47,7 @@ impl Display {
         backlight_pin.into_push_pull_output();
         lcd_dc_pin.into_push_pull_output();
         lcd_cs_pin.into_push_pull_output();
+        lcd_cs_pin.set_low().unwrap();
         lcd_sck_pin
             .try_into_mode(DynPinMode::Function(DynFunction::Spi))
             .unwrap();
@@ -59,29 +60,48 @@ impl Display {
             resets,
             125_000_000u32.Hz(),
             62_500_000u32.Hz(),
-            &MODE_0,
+            &MODE_3,
         );
-        let di = SPIInterface::new(spi, lcd_dc_pin, lcd_cs_pin);
+        let di = SPIInterfaceNoCS::new(spi, lcd_dc_pin);
         let mut st7789 = ST7789::new(di, lcd_reset_pin, WIDTH as u16, HEIGHT as u16);
         st7789.init(delay_source).unwrap();
-        st7789.clear(Rgb565::BLACK).unwrap();
         let mut display = Display {
             st7789,
             backlight_pin,
             framebuffer: [0; WIDTH * HEIGHT],
         };
+        let colors =
+            core::iter::repeat(RawU16::from(Rgb565::BLACK).into_inner()).take(WIDTH * HEIGHT);
+        display
+            .st7789
+            .set_pixels(0, 0, (WIDTH - 1) as u16, (HEIGHT - 1) as u16, colors)
+            .unwrap();
         display.enable_backlight();
         display
     }
 
     pub fn flush(&mut self) {
-        self.st7789
-            .set_pixels(0, 0, WIDTH as u16, HEIGHT as u16, self.framebuffer)
-            .unwrap();
+        unsafe {
+            let dma_base = 0x50000000 as *mut u32;
+            let spi0_tx = 0x4003c000 + 8;
+            let ch0_read_addr = dma_base.offset(0);
+            let ch0_write_addr = dma_base.offset(1);
+            let ch0_trans_count = dma_base.offset(2);
+            let ch0_ctrl_trig = dma_base.offset(3);
+            ch0_read_addr.write_volatile(self.framebuffer.as_ptr() as u32);
+            ch0_write_addr.write_volatile(spi0_tx);
+            ch0_trans_count.write_volatile((WIDTH * HEIGHT * 2) as u32);
+            ch0_ctrl_trig.write_volatile((1 << 0) | (0 << 2) | (1 << 4) | (16 << 15));
+            while ch0_trans_count.read_volatile() > 0 {}
+        }
     }
 
     pub fn enable_backlight(&mut self) {
         self.backlight_pin.set_high().unwrap();
+    }
+
+    pub fn disable_backlight(&mut self) {
+        self.backlight_pin.set_low().unwrap();
     }
 }
 
@@ -98,7 +118,7 @@ impl DrawTarget for Display {
             if let Ok((x @ 0..=M, y @ 0..=M)) = coord.try_into() {
                 let index: u32 = x + y * WIDTH as u32;
                 let color = RawU16::from(color).into_inner();
-                self.framebuffer[index as usize] = color;
+                self.framebuffer[index as usize] = color.to_be();
             }
         }
 
