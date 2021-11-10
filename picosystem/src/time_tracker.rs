8b7b4d337cafe0a40e8e6e21b5114390fd4ea56d
Rich Lane <lanerl@gmail.com>
2021-11-09 20:29:53 -0800

    time_tracker: add RAII api

--- picosystem/src/time_tracker.rs
+++ picosystem/src/time_tracker.rs
@@ -21,20 +21,40 @@ impl TimeTracker {
     pub fn run<F: FnOnce()>(&mut self, func: F) {
         let start = time::time_us();
         func();
-        let end = time::time_us();
-        let elapsed = end - start;
+        self.update(time::time_us() - start);
+    }
+
+    pub fn start<'a>(&'a mut self) -> Tracked<'a> {
+        Tracked {
+            tracker: self,
+            start: time::time_us(),
+        }
+    }
+
+    pub fn update(&mut self, elapsed: u32) {
         self.accumulated_us += elapsed;
         self.count += 1;
-        if end - self.last_display_us >= 1_000_000 {
+        if time::time_us() - self.last_display_us >= 1_000_000 {
             info!(
                 "TimeTracker {}: {} us/call {} us/sec",
                 &self.name,
                 self.accumulated_us / self.count,
-                (self.accumulated_us * 1000) / ((end - self.last_display_us) / 1000)
+                (self.accumulated_us * 1000) / ((time::time_us() - self.last_display_us) / 1000)
             );
-            self.last_display_us = end;
+            self.last_display_us = time::time_us();
             self.count = 0;
             self.accumulated_us = 0;
         }
+    }
+}
+
+pub struct Tracked<'a> {
+    tracker: &'a mut TimeTracker,
+    start: u32,
+}
+
+impl Drop for Tracked<'_> {
+    fn drop(&mut self) {
+        self.tracker.update(time::time_us() - self.start);
     }
 }
