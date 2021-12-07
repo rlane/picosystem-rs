474bfedcb417b79efa8f952c35eabbaa9edec3dd
Rich Lane <lanerl@gmail.com>
2021-12-06 19:04:00 -0800

    time: add 64-bit time method

--- picosystem/src/time.rs
+++ picosystem/src/time.rs
@@ -1,3 +1,10 @@
 pub fn time_us() -> u32 {
     unsafe { (*rp2040_pac::TIMER::PTR).timerawl.read().bits() }
 }
+
+pub fn time_us64() -> u64 {
+    unsafe {
+        (*rp2040_pac::TIMER::PTR).timelr.read().bits() as u64
+            | (((*rp2040_pac::TIMER::PTR).timehr.read().bits() as u64) << 32)
+    }
+}
